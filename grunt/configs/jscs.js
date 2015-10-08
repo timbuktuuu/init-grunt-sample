d0d112399ccf651ae67a41960bf6cbe6470061b5
jnachtigall <jnachtigall@init.de>
2015-10-08 14:01:51 +0200

    Added jshint syntac and jscs code style checker

---
+++ grunt/configs/jscs.js
@@ -0,0 +1,33 @@
+/**
+ * This plugin will compress and mangle the input files using
+ * the provided options.
+ *
+ * ---------------------------------------------------------------
+ *
+ * This plugin requires Grunt >=0.4.0
+ *
+ * For usage docs see:
+ * 		https://github.com/gruntjs/grunt-jscs
+ *
+ */
+
+'use strict';
+module.exports = function(grunt) {
+  grunt.config.set('jscs', {
+    src:  [
+      'Gruntfile.js',
+      'grunt/**/*.js',
+       require('../pipeline').jsFilesToInject
+    ],
+    options: {
+      config: '.jscsrc',
+      // must be the same as in .jshintignore
+      excludeFiles: [
+        'src/js/vendor/**'
+      ]
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-jscs');
+
+};
